142ac2414544114941bb492a10b074f966b25335
HaiVy-coder <buiduonghaivy@gmail.com>
2025-10-05 15:41:15 +0700

    bai2_thiep_mung_sinh_nhat - chinh sua lan 2

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from guizero import App, Text, Picture
 cuaso = App("Thiep chuc mung", width = 700, height = 750)
 chu = Text(cuaso, "Chuc Mung Sinh Nhat")
 picture = Picture(cuaso, "banh sinh nhat.png")
-chu2 = Text(cuaso, "Bui Duong Hai Vy")
+chu2 = Text(cuaso, "Chuc ban co mot ngay sinh nhat tuyet voi!")
+chu3 = Text(cuaso, "Tu Bui Duong Hai Vy")
 cuaso.display()
