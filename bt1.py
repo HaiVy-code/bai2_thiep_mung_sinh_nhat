2eb89baf70d1116c3376f49ea0daf31ddf619ac1
HaiVy-coder <buiduonghaivy@gmail.com>
2025-10-05 16:11:53 +0700

    Update bt1.py

--- bt1.py
+++ bt1.py
@@ -1,6 +1,1 @@
-from guizero import App, Text, Picture
-cuaso = App("Thiep moi", width = 1200, height = 700, bg = "#FF7B7B")
-chu = Text(cuaso, "Moi ban den du le hoi Tet Trung Thu", color = "yellow")
-picture = Picture(cuaso, "tet trung thu.png")
-chu2 = Text(cuaso, "Tu Bui Duong Hai Vy", color = "yellow")
-cuaso.display()
+
